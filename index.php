3df670cb1f20df3360e0d9454aa0d1807320c4ab
Raphoudre <raphael.ivol@hotmail.fr>
2021-01-28 16:59:36 +0100

    ajout de la structure de l'investomium

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>INVESTOMIUM</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Londrina+Solid:wght@300&family=Rubik&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row m-2 border-bottom">
+            <div class="col-2 text-center">
+                <h1 class="fs-1 text-bold">Investomium</h1>
+            </div>
+            <div class="col-10 d-flex justify-content-end">
+                <button class="btn btn-primary m-1">connexion</button>
+            </div>
+        </div>
+        <div class="row m-2">
+            <div class="col-12">
+                <a href="keske.php">Qu'est-ce que l'<strong>Investomium</strong> ?</a>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>    
+</body>
+</html>
---
+++ keske.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>kézakomium ?</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/keskestyle.css">
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row border-bottom text-center"><h1>L'INVESTOMIUM, QU'EST-CE ?</h1></div>
+        <div class="jumbotron jumbotron-fluid m-5 bg-lildark shadow rounded">
+            <div class="container">
+                <p class="fs-3">L'Investomium, est un site vous permettant d'investir sur des entités moléculaires,
+                </br> très rares mais surpuissantes. Ces entités font parties de la famille des Entimium, des molécules gardées secrètes jusqu'en 2019.
+                </p><hr><p class="fs-3"> Jusqu'au jour où un célèbre scientifique dénomé "Dr. Biggdradonamous Uvuvwevwevwe Onyetenyevwe Ugwemubwem Ossas (3ème du nom)", découvra
+                l'Obamium de façon publique le 3 Octobre 2019 (voir photo)</br>
+                <img src="assets/img/illustratiomium/finallyobamium.jpg" alt="" height="150rem"></p>
+                <hr><p class="fs-3">L'obamium est l'un des éléments les plus difficiles à recevoir un isotope neutre en raison de sa séquence de désintégration radioactive rapide. Bien que sa demi-vie soit de 48 millisecondes, son invocation peut avoir un impact pendant huit ans en raison de son rayonnement. Une fois stabilisé et décomposé, il peut se présenter sous sa forme la plus basique sous la forme d'un solide malléable brun capable de conduire l'électricité, qui sous cette forme est utilisé pour créer un type spécial de conducteur électrique qui peut permettre le passage d'électrons 23,5% plus rapidement que l'argent. Les inventions qui en utilisent sont celles qui nécessitent une énergie très élevée, comme le fauteuil roulant Mecha et le réservoir cyber I.S.I.S.<p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
